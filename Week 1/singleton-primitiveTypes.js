0e7ab27ce13ef7ec8b40745a11fc8cf5414620cc
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-05-31 09:27:58 -0500

    Add file singleton-primitiveTypes.js

---
+++ Week 1/singleton-primitiveTypes.js
@@ -0,0 +1,12 @@
+var firstName = "Justin"
+var lastName = "Singleton"
+var age = "23"
+var street = "15 Sawyer Ct."
+var city = "Phoenix"
+var zipCode = "12345"
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(street)
+console.log(city)
+console.log(zipCode)
