2aa1515f46641cbf71c4c4c632c546a869606a36
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-05-31 09:32:21 -0500

    Add attribution to singleton-primitiveTypes.js

--- Week 1/singleton-primitiveTypes.js
+++ Week 1/singleton-primitiveTypes.js
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Justin Singleton 
+; Date: 31 May 2019
+; Modified By: N/A
+; Description: This program is to show 
+; competency in variables and primitieve 
+; types.
+;===========================================
+*/
 var firstName = "Justin"
 var lastName = "Singleton"
 var age = "23"
