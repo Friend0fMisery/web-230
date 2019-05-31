6dd5c86045d7be201dd2d4af15f0756c2a9147fe
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-05-31 09:12:42 -0500

    Add attribution line to 1.3

--- Week 1/hello-world.js
+++ Week 1/hello-world.js
@@ -1,1 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Justin Singleton 
+; Date: 25 June 2017
+; Modified By: N/A
+; Description: This program is to show 
+; competency of Assignment and Git.
+;===========================================
+*/
 console.log('Hello, World. My name is Justin Singleton.')
