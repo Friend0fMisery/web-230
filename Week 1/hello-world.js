aaee1b40c2b932911701c1eb45fe6ba36217d1c4
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-05-31 09:13:45 -0500

    Change date.

--- Week 1/hello-world.js
+++ Week 1/hello-world.js
@@ -2,7 +2,7 @@
 ============================================
 ; Title: Assignment 1.3
 ; Author: Justin Singleton 
-; Date: 25 June 2017
+; Date: 30 May 2019
 ; Modified By: N/A
 ; Description: This program is to show 
 ; competency of Assignment and Git.
