aa19c78dd21ae1b2b60ecacea36aff078afe3fea
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-06-06 19:16:18 -0500

    Add files for Week-1 and Week-2

---
+++ Week 2/singleton-header.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  header.js
+; Author: Justin Singleton
+; Date:   25 June 2017
+; Modified By: N/A
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+exports.display = function (firstName, lastName, assignment) {
+	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+	new Date().toLocaleDateString('en-US')
+
+	return output
+}
---
+++ Week 2/singleton-header.test.js
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Justin Singleton
+; Date: 25 June 2019
+; Description: Displays a formatted header.
+;===========================================
+*/
+const header = require('./singleton-header.js');
+
+console.log(header.display("Justin", "Singleton", "Week 2"));
